94fe4df433d4a6b570837a9fb79a5ebc7a76b440
mirabilos <m@mirbsd.org>
2020-11-30 00:10:32 +0100

    regenerate all static HTML files from tpl/ now

--- mkhtm.sh
+++ mkhtm.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 #-
-# Tool to recreate marker.htm from index.htm
+# Tool to recreate index.htm and marker.htm from tpl/
 
 set -ex
 cd "$(realpath "$0/..")"
+cat \
+    tpl/0-prefix.htm \
+    tpl/1-defloc.js \
+    tpl/5-hookfn.js \
+    tpl/9-suffix.htm \
+    >index.htm~
+mv index.htm~ index.htm
 sed \
     -e '/<title>/s^.*$ <title>MirKarte with multiple markers (Alpha)</title>' \
     -e 's/mirkarte\.js/marker.js/g' \
