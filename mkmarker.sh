f8dd116d6dac8cc1d1cff9f83086b0907a75c18b
mirabilos <m@mirbsd.org>
2020-11-29 23:58:13 +0100

    fix dirname-of-$0 in symlinked cases

--- mkmarker.sh
+++ mkmarker.sh
@@ -3,7 +3,7 @@
 # Tool to recreate marker.htm from index.htm
 
 set -ex
-cd "$(dirname "$0")"
+cd "$(realpath "$0/..")"
 sed \
     -e '/<title>/s^.*$ <title>MirKarte with multiple markers (Alpha)</title>' \
     -e 's/mirkarte\.js/marker.js/g' \
